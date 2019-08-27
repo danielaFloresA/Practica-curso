9113bf9e82a86359c6148795b1280650085079fb
Daniela <danielafloresavila.02@gmail.com>
2019-08-26 21:09:34 -0400

    Create lunesReact.js

---
+++ lunesReact.js
@@ -0,0 +1,23 @@
+// Ejercicio 1
+let people = ['Steve', 'Pete', 'Mike','Nick'];
+let imprimir = ['Marcos','inicio',...people];
+console.log(imprimir);
+//Ejercicio 2
+let user = { 
+    name:'Steve',
+    age:22
+}
+let details = {
+    place : 'Romania',
+    hobby : 'reading'
+}
+
+console.log(`unir ${place} con ${hobby}`);
+//Ejercicio 3
+
+let user = {
+    name : 'Pedro',
+    lastname : 'Poveda',
+    details : {phone: 4553120, dir:'direction 123'}
+}
+console.log(details);
